1191a8847cc8c6b4838d01dd045cc0b0cea8533f
Samuel Alves <samuel.estudo778@gmail.com>
2022-02-15 13:07:07 -0300

    add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import './global/GlobalStyles.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
